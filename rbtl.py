95f0aceb7eafa47372cb2bd3db053bb18cce73d8
mitchellkarmen <mitchkarmen@yahoo.com>
2022-07-22 15:45:46 -0700

    brackets

--- rbtl.py
+++ rbtl.py
@@ -46,7 +46,7 @@ def negative_log_likelihood_multiple(params, max_flux, fluxerr, color_law):
 
 	To implement this:
 	opt = minimize(negative_log_likelihood_multiple,
-				np.zeros(2, len(self.maximum_flux)),
+				np.zeros([2, len(self.maximum_flux)]),
 				args = (self.maximum_flux,
 						self.maximum_fluxerr,
 						self.rbtl_color_law))
@@ -61,19 +61,3 @@ def negative_log_likelihood_multiple(params, max_flux, fluxerr, color_law):
 	sigma_sq = (intrinsic_dispersion * mean_flux)**2 + scale_fluxerr**2
 
 	return np.sum(0.5 * (scale_flux - mean_flux)**2 / (sigma_sq) + np.log(np.sqrt(sigma_sq)))
-
-# To implement:
-# from scipy import minimize
-# self.fractional_dispersion = np.std(self.maximum_flux, axis = 0) / np.mean(self.maximum_flux, axis=0)
-# mean_flux = np.mean(self.maximum_flux, axis=0)
-# for i in range(len(self.maximum_flux)):
-#     opt = minimize(negative_log_likelihood, 
-#             [0., 0.], 
-#             args = (self.maximum_flux[i],
-#                 self.maximum_fluxerr[i],
-#                 self.rbtl_color_law,
-#                 mean_flux,
-#                 self.fractional_dispersion)
-#             )
-#     print(self.targets[i])
-#     print(opt.x)
