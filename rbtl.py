0be6be26056a0cda3b3e3ec37dfcf9e293f7a2b9
mitchellkarmen <mitchkarmen@yahoo.com>
2022-07-22 15:33:23 -0700

    added multiple sn rbtl

--- rbtl.py
+++ rbtl.py
@@ -10,6 +10,40 @@ def negative_log_likelihood(params, max_flux, fluxerr, color_law, mean_flux, int
 
 	return np.sum(0.5 * (scale_flux - mean_flux)**2 / (sigma_sq) + np.log(np.sqrt(sigma_sq)))
 
+
+def negative_log_likelihood_multiple(params, max_flux, fluxerr, color_law):
+	"""
+	Parameters
+	----------
+		params: 
+			unpacks to dm, color.  dm is shape [N_SNe], color is [N_SNe]
+		max_flux:
+			all max flux spectra, shape is [N_SNe, N_bins]
+		fluxerr:
+			corresponds to max flux
+		color_law:
+			fitzgerald dust law
+
+
+
+	To implement this:
+	opt = minimize(negative_log_likelihood_multiple,
+				np.zeros(2, len(self.maximum_flux)),
+				args = (self.maximum_flux,
+						self.maximum_fluxerr,
+						self.rbtl_color_law))
+	print(opt.x)
+	"""
+	dm, color = params
+	scale = 10**(0.4 * (dm + color * color_law))
+	scale_flux = max_flux * scale
+	scale_fluxerr = fluxerr * scale
+	mean_flux = np.mean(scale_flux, axis=0)
+	intrinsic_dispersion = np.std(scale_flux, axis=0) / mean_flux
+	sigma_sq = (intrinsic_dispersion * mean_flux)**2 + scale_fluxerr**2
+
+	return np.sum(0.5 * (scale_flux - mean_flux)**2 / (sigma_sq) + np.log(np.sqrt(sigma_sq)))
+
 # To implement:
 # from scipy import minimize
 # self.fractional_dispersion = np.std(self.maximum_flux, axis = 0) / np.mean(self.maximum_flux, axis=0)
