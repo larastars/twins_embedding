2f48671bf7112375225659b546d433633673a1f8
mitchellkarmen <mitchkarmen@yahoo.com>
2022-07-22 15:40:41 -0700

    move comment to docstring

--- rbtl.py
+++ rbtl.py
@@ -2,6 +2,24 @@ import numpy as np
 import extinction
 
 def negative_log_likelihood(params, max_flux, fluxerr, color_law, mean_flux, intrinsic_dispersion):
+	"""
+	 To implement:
+	from scipy import minimize
+	self.fractional_dispersion = np.std(self.maximum_flux, axis = 0) / np.mean(self.maximum_flux, axis=0)
+	mean_flux = np.mean(self.maximum_flux, axis=0)
+	for i in range(len(self.maximum_flux)):
+	    opt = minimize(negative_log_likelihood, 
+	            [0., 0.], 
+	            args = (self.maximum_flux[i],
+	                self.maximum_fluxerr[i],
+	                self.rbtl_color_law,
+	                mean_flux,
+	                self.fractional_dispersion)
+	            )
+	    print(self.targets[i])
+	    print(opt.x)
+
+    """
 	dm, color = params
 	scale = 10**(0.4 * (dm + color * color_law))
 	scale_flux = max_flux * scale
