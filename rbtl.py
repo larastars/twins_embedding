7a7e5814b900b3475f355bf7a94c05f6a1f71644
mitchellkarmen <mitchkarmen@yahoo.com>
2022-08-09 12:13:19 -0400

    using eq 7 instead of 10

--- rbtl.py
+++ rbtl.py
@@ -21,10 +21,10 @@ def negative_log_likelihood(params, max_flux, fluxerr, color_law, mean_flux, int
 
     """
 	dm, color = params
-	scale = 10**(0.4 * (dm + color * color_law))
-	scale_flux = max_flux * scale
-	scale_fluxerr = fluxerr * scale
-	sigma_sq = (intrinsic_dispersion * mean_flux)**2 + scale_fluxerr**2
+	scale = 10**(-0.4 * (dm + color * color_law))
+	scale_flux = mean_flux * scale
+	#scale_fluxerr = fluxerr * scale
+	sigma_sq = (intrinsic_dispersion * scale_flux)**2 + fluxerr**2
 
 	return np.sum(0.5 * (scale_flux - mean_flux)**2 / (sigma_sq) + np.log(np.sqrt(sigma_sq)))
 
